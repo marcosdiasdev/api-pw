10ac81e85a527dc4ae65b6d6a9d40d00ac8103bc
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2021-11-28 19:06:49 -0300

    Corrige seed de fabricantes

--- database/seeds/1_fabricantes_seed.js
+++ database/seeds/1_fabricantes_seed.js
@@ -1,15 +1,18 @@
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex("fabricantes")
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex("fabricantes").insert([
-        { nome: "Dell" },
-        { nome: "Apple" },
-        { nome: "Acer" },
-        { nome: "Asus" },
-        { nome: "HP" },
-      ]);
-    });
+exports.seed = async function (knex) {
+  // Verifica se existem produtos cadastrados antes de recriar os fabricantes
+  const produtos = knex("produtos");
+  if (produtos.length === 0) {
+    return knex("fabricantes")
+      .del()
+      .then(function () {
+        // Inserts seed entries
+        return knex("fabricantes").insert([
+          { nome: "Dell" },
+          { nome: "Apple" },
+          { nome: "Acer" },
+          { nome: "Asus" },
+          { nome: "HP" },
+        ]);
+      });
+  }
 };
